673e502ce944363c9d9ca3494bf3b44773523788
Thomas Chen <k1@ucla.edu>
2016-11-01 10:09:21 -0700

    Unpacking boost takes 15 minutes so lets just cache it and build in next round.

--- build-boost.sh
+++ build-boost.sh
@@ -44,22 +44,23 @@ if [ "$BUILD_BOOST" = true ]; then
     time brew unlink boost
     if [ ! -d boost-$BOOSTVER ]; then
         time brew unpack --patch --destdir=. boost;
-    fi
-    pushd boost-$BOOSTVER;
-    echo "Bootstrap boost..."
-    time ./bootstrap.sh --prefix=/usr/local/Cellar/boost/$BOOSTVER --libdir=/usr/local/Cellar/boost/$BOOSTVER/lib --without-icu --without-libraries=python,mpi > boost_bootstrap.log
-    {
-    echo "using darwin : : /usr/local/llvm/bin/clang++"
-    echo "             : <cxxflags>$MACOS_SDK <linkflags>$MACOS_SDK <compileflags>$MACOS_SDK ;"
-    echo "using python : 3.5"
-    echo "             : /usr/local/bin/python3.5"
-    echo "             : /usr/local/Cellar/python3/$PYTHONVER/include/python3.5m ;"
-    } > user-config.jam;
-    echo 'boost headers...'
-    time ./b2 headers
-    echo "Building boost..."
-    travis_wait 40 ./b2 --prefix=/usr/local/Cellar/boost/$BOOSTVER --libdir=/usr/local/Cellar/boost/$BOOSTVER/lib -d2 -j4 --layout=tagged --user-config=user-config.jam install threading=multi,single link=shared,static;
-    popd;
-    brew link --overwrite boost
-    brew bottle boost;
+    else
+        pushd boost-$BOOSTVER;
+        echo "Bootstrap boost..."
+        time ./bootstrap.sh --prefix=/usr/local/Cellar/boost/$BOOSTVER --libdir=/usr/local/Cellar/boost/$BOOSTVER/lib --without-icu --without-libraries=python,mpi > boost_bootstrap.log
+        {
+        echo "using darwin : : /usr/local/llvm/bin/clang++"
+        echo "             : <cxxflags>$MACOS_SDK <linkflags>$MACOS_SDK <compileflags>$MACOS_SDK ;"
+        echo "using python : 3.5"
+        echo "             : /usr/local/bin/python3.5"
+        echo "             : /usr/local/Cellar/python3/$PYTHONVER/include/python3.5m ;"
+        } > user-config.jam;
+        echo 'boost headers...'
+        time ./b2 headers
+        echo "Building boost..."
+        travis_wait 40 ./b2 --prefix=/usr/local/Cellar/boost/$BOOSTVER --libdir=/usr/local/Cellar/boost/$BOOSTVER/lib -d2 -j4 --layout=tagged --user-config=user-config.jam install threading=multi,single link=shared,static;
+        popd;
+        brew link --overwrite boost
+        brew bottle boost;
+    fi;
 fi;
